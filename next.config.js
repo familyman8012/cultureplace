5406067aec2b4b7c166cb033f2ffa8f59a304bb1
familyman <familyman801205@gmai.com>
2022-03-27 05:58:55 +0900

    swc 제거

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: ["kopis.or.kr", "cdn.class101.net"],
     formats: ["image/avif", "image/webp"]
